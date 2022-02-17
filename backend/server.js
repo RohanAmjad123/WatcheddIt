f15292bc4a1482ee2d3da902dd7165144120e753
Karl Winkler <39600307+KarlWinkler@users.noreply.github.com>
2022-02-16 17:43:54 -0700

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,40 @@
 const express = require("express");
+const mongodb = require("mongodb");
 const bodyParser = require("body-parser");
 
 const app = express();
-
-app.set('view engine', 'react');
-
-app.listen(3000, function(){
-    console.log("server started");
+const connection = "mongodb+srv://user2:seng401@cluster0.bgqcz.mongodb.net/test";
+const client = new MongoClient(connection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-app.get('/', function(req, res){
-    res.json({
-        status: 'API online',
-        message: 'Welcome'
+let dbconnection;
+
+function connectToServer(callback) {
+    client.connect(function (err, db) {
+      if (err || !db) {
+        return callback(err);
+      }
+
+      dbConnection = db.db("sample_airbnb");
+      console.log("Successfully connected to MongoDB.");
+
+      return callback();
     });
-});
+};
+
+function getDb() {
+    return dbConnection;
+};
+
+// app.listen(3000, function(){
+//     console.log("server started");
+// });
+
+// app.get('/', function(req, res){
+//     res.json({
+//         status: 'API online',
+//         message: 'Welcome'
+//     });
+// });
