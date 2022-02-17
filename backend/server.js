d51ee4343f88f5f1c720d7c4a6d93a280c749418
Karl Winkler <39600307+KarlWinkler@users.noreply.github.com>
2022-02-16 18:12:07 -0700

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongodb = require("mongodb");
+const {MongoClient} = require("mongodb");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -14,13 +14,13 @@ let dbconnection;
 function connectToServer(callback) {
     client.connect(function (err, db) {
       if (err || !db) {
-        return callback(err);
+        // return callback(err);
       }
 
       dbConnection = db.db("sample_airbnb");
       console.log("Successfully connected to MongoDB.");
 
-      return callback();
+    //   return callback();
     });
 };
 
@@ -28,13 +28,30 @@ function getDb() {
     return dbConnection;
 };
 
-// app.listen(3000, function(){
-//     console.log("server started");
-// });
+app.listen(3000, function(){
+    console.log("server started");
+});
 
-// app.get('/', function(req, res){
-//     res.json({
-//         status: 'API online',
-//         message: 'Welcome'
-//     });
-// });
+app.get('/', function(req, res){
+    
+    const dbConnect = getDb();
+    const matchDocument = {
+      listing_id: 10006542,
+      last_modified: new Date(),
+      session_id: 1,
+      direction: "req.body.direction"
+    };
+  
+    dbConnect
+    .collection("listingsAndReviews")
+    .find({}).limit(50)
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching listings!");
+     } else {
+        res.json(result);
+      }
+    });
+});
+
+connectToServer();
