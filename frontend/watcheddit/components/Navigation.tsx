ae649098f07818473d30d7fc9a53dc19f34222e1
Rohan Amjad <rohanamjad123@gmail.com>
2022-02-17 19:06:06 -0700

    created basic navigation

--- frontend/watcheddit/components/Navigation.tsx
+++ frontend/watcheddit/components/Navigation.tsx
@@ -1,15 +1,18 @@
 import { AppBar } from '@mui/material';
+import { Toolbar } from '@mui/material';
 import { Grid } from '@mui/material';
 import { Typography } from '@mui/material';
 
 export default function Navigation() {
     return (
-        <AppBar>
-            <Grid container>
-                <Grid item>
-                    <Typography>Watcheddit</Typography>
-                </Grid>
-            </Grid>
+        <AppBar enableColorOnDark sx={{ bgcolor: 'white' }} position='sticky'>
+            <Toolbar>
+                <Grid container>
+                    <Grid item>
+                        <Typography sx={{ color: 'text.primary' }}>Watcheddit</Typography>
+                    </Grid>
+                </Grid> 
+            </Toolbar>
         </AppBar>
     );
 }
