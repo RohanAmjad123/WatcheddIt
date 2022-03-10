3d40d9d28b483884c96740aab723c3479d43a5d5
Rohan Amjad <rohanamjad123@gmail.com>
2022-03-10 00:54:26 -0700

    index page changes

--- frontend/watcheddit/pages/index.tsx
+++ frontend/watcheddit/pages/index.tsx
@@ -1,13 +1,11 @@
 import { Grid } from "@mui/material";
-import Media from "../components/Media";
 import { Container } from "@mui/material";
 
-export default function home({ media }) {
-  const mediaList = media.map((m) => <Media key={m.id} media={m} />);
-
+export default function home({ media }: {media:any[]}) {
+  const mediaList = media.map((m) => <MediaCard key={m.id} media={m} />);  
   return (
     <Container>
-      <Grid container>{mediaList}</Grid>
+      <Grid container direction='column'>{ mediaList }</Grid>
     </Container>
   );
 }
