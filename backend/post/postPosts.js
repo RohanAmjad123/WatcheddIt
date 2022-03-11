19c789f13ea392b3eafbb9907caf29add6e5431c
Rohan Amjad <rohanamjad123@gmail.com>
2022-03-11 00:31:42 -0700

    commented out sessions if condition

--- backend/post/postPosts.js
+++ backend/post/postPosts.js
@@ -1,16 +1,15 @@
 const connect = require("../database.js");
 
 exports.postPost = (req, res) => {
-    if(req.session.user){
+    //if(req.session.user){
         const dbConnect = connect.getWriteDb();
 
         dbConnect
         .collection("posts")
         .insertOne(req.body);
-
         res.sendStatus(200);
-    }
-    else{
-        res.status(400).send("Can't POST post, not logged in");
-    }
+    //}
+    //else{
+        //res.status(400).send("Can't POST post, not logged in");
+    //}
 }
