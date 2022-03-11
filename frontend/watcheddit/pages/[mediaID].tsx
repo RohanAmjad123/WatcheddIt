782377295119b37a1edc68ced80f12083327d903
Rohan Amjad <rohanamjad123@gmail.com>
2022-03-10 19:17:31 -0700

    added getStaticPaths and getStaticProps for dynamic routing and data fetching

--- frontend/watcheddit/pages/[mediaID].tsx
+++ frontend/watcheddit/pages/[mediaID].tsx
@@ -1,11 +1,48 @@
-import { useRouter } from 'next/router';
+import MediaCard from "../components/MediaCard";
+import { Container } from "@mui/material";
+import { Grid } from "@mui/material";
 import Typography from '@mui/material/Typography';
 
-export default function MediaPage() {
-    const router = useRouter();
-    const { mediaID } = router.query
-    console.log(mediaID)
+export default function MediaPage({ mediaList, postsList }: { mediaList: any[], postsList: any[] }) {
+    const mediaCard = mediaList.map((m) => <MediaCard key={ m.imdbID } media={ m }/>);
+    const postCards = postsList.map((p) => { p.Description });
+    console.log(mediaCard);
     return (
-        <Typography>{mediaID}</Typography>
+        <Container>
+            <Grid container direction="column">
+                <Grid item>{ mediaCard }</Grid>
+                <Grid item>   
+                </Grid>
+            </Grid>
+        </Container>
     );
+}
+
+export async function getStaticPaths() {
+    const res = await fetch(`http://localhost:3000/api/media`);
+    const mediaList = await res.json();
+
+    const paths = mediaList.map((m: any) => ({
+        params: { mediaID: m.imdbID },
+    }))
+    
+    return {
+        paths, 
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params } : { params: any }) {
+    const mediaRes = await fetch(`http://localhost:3000/api/${params.mediaID}`);
+    const mediaList = await mediaRes.json();
+
+    const postRes = await fetch(`http://localhost:3000/api/media/${params.mediaID}/posts/`);
+    const postsList = await postRes.json();
+
+    return {
+        props: {
+            mediaList, 
+            postsList,
+        }
+    };
 }
