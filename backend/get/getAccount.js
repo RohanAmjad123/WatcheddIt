046ea82100b87288dba8545967e4d5dd89f6853c
sajid <Sajid2437@gmail.com>
2022-03-23 22:15:04 -0600

    added b-crpy password security
    passwords now hash with b-crypt library

--- backend/get/getAccount.js
+++ backend/get/getAccount.js
@@ -1,26 +1,44 @@
 const connect = require("../database.js");
+var bcrypt = require('bcrypt');
 
 exports.login = (req, res) => {
     const dbConnect = connect.getDb();
+    console.log("Logging into an account...")
     dbConnect.collection("users").findOne(
         {
             username: req.body.username,
-            password: req.body.password
-        }, function(err, result) {
+        }, function(err, result) 
+        {
             if (err) throw err;
             console.log(result)
             if (result == null) {
-                console.log("User not found")
-                res.status(401);
-            } else if (result.type === "admin") {
-                console.log("user is admin")
-                req.session.admin = result;
-                res.send(result)
-            } else {
-                console.log("User is regular user")
-                req.session.user = result;
-                res.send(result)
+                console.log("No matching email-address")
+                res.status(400).send("No matching email-address found")
+                return
             }
+            bcrypt.compare(req.body.password, result.password, function (err1, res2) 
+            {
+                if (err1) {
+                    console.log("An error has occured while trying to login")
+                    res.status(400).send("An error has occured while trying to login")
+                    throw err1;
+                }
+                if (res2 == false) {
+                    console.log("Wrong password!")
+                    res.status(400).send("Wrong password")
+                    return;
+                }
+                // A valid password hash match has been found
+                if (result.type == "admin") {
+                    console.log("user is admin")
+                    req.session.admin = result;
+                    res.send(result)
+                } else {
+                    console.log("user is regular user")
+                    req.session.user = result
+                    res.send(result)
+                }
+            })
         }
     )
 }
--- backend/post/postAccount.js
+++ backend/post/postAccount.js
@@ -1,21 +1,25 @@
 const { response } = require("express");
 const connect = require("../database.js");
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 exports.signup = (req, res) => {
     console.log("Registering an account");
     const dbConnect = connect.getDb();
-    dbConnect.collection("users").insertOne(
-        {
-            username: req.body.username,
-            password: req.body.password,
-            type: "user",
-            date: Date.now
-        }, function(err, response) {
-            if (err) {
-                res.status(400).send(response)
-            } else {
-                res.send(response)
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        dbConnect.collection("users").insertOne(
+            {
+                username: req.body.username,
+                password: hash,
+                type: "user",
+                date: Date.now
+            }, function(err, response) {
+                if (err) {
+                    res.status(400).send(response)
+                } else {
+                    res.send(response)
+                }
             }
-        }
-    )
+        )    
+    })
 }
