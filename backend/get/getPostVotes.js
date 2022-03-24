25638e70aefe7acc22159ec46f864526c7062493
Kaumil Patel <spdmode@gmail.com>
2022-03-23 22:18:28 -0600

    Update getPostVotes.js

--- backend/get/getPostVotes.js
+++ backend/get/getPostVotes.js
@@ -5,35 +5,33 @@ exports.getPostVotes = (req, res) => {
     console.log("getPostVotes " + req.params.postID)
     const dbConnect = connect.getDb();
     dbConnect.collection("PostVotes")
-        .aggregate([
-            {
-                '$match': {
-                    'postID': ObjectId(req.params.postID)
-                }
-            }, {
-                '$group': {
-                    '_id': '$postID',
-                    'upVote': {
-                        '$sum': {
-                            '$cond': [
-                                '$vote', 1, 0
-                            ]
-                        }
-                    },
-                    'downVote': {
-                        '$sum': {
-                            '$cond': [
-                                '$vote', 0, 1
-                            ]
-                        }
+        .aggregate([{
+            '$match': {
+                'postID': ObjectId(req.params.postID)
+            }
+        }, {
+            '$group': {
+                '_id': '$postID',
+                'upVote': {
+                    '$sum': {
+                        '$cond': [
+                            '$vote', 1, 0
+                        ]
+                    }
+                },
+                'downVote': {
+                    '$sum': {
+                        '$cond': [
+                            '$vote', 0, 1
+                        ]
                     }
                 }
-            }, {
-                '$project': {
-                    '_id': 0
-                }
             }
-        ])
+        }, {
+            '$project': {
+                '_id': 0
+            }
+        }])
         .toArray()
         .then(items => {
             res.json(items);
@@ -52,8 +50,7 @@ exports.getUserPostVotes = (req, res) => {
         const postID = req.params.postID;
         const userID = req.session.user._id;
         dbConnect.collection("PostVotes")
-            .find({"postID": ObjectId(postID), "userID": ObjectId(userID)}, {projection: {_id: 0, vote: 1}})
-            .toArray()
+            .findOne({"postID": ObjectId(postID), "userID": ObjectId(userID)}, {projection: {_id: 0, vote: 1}})
             .then(items => {
                 res.json(items);
                 res.sendStatus(200);
