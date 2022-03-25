efc32aed3482a4ac604fd98274cc03881fd6a51a
Rohan Amjad <rohanamjad123@gmail.com>
2022-03-25 02:05:02 -0600

    added comment form

---
+++ frontend/watcheddit/forms/CommentForm.tsx
@@ -0,0 +1,57 @@
+import { Grid, FormControl, TextField, Button, Paper } from '@mui/material'
+import axios from 'axios'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { useAppSelector } from '../app/hooks'
+
+const defaultFormValues = {
+    text: "",
+}
+
+export default function CommentForm() {
+    const [formValues, setFormValues] = useState(defaultFormValues)
+    const router = useRouter();
+    const { mediaID, postID } = router.query
+    const username = useAppSelector((state) => state.username)
+
+    const handleChange = (event: any) => {
+        setFormValues({
+            ...formValues,
+            text: event.target.value
+        })
+    }
+
+    const handleClick = () => {
+        const comment = {
+            user: username,
+            text: formValues.text,
+            postID: postID
+        }
+
+        axios.post(`http://localhost:3000/api/comment/${postID}/add`, comment, { withCredentials: true })
+            .then((response) => {
+
+            },
+                (error) => {
+                    console.log(error)
+                })
+
+        setFormValues({
+            ...formValues,
+            text: ""
+        })
+    }
+
+    return (
+        <Paper>
+            <Grid container p={2} direction="row" columnSpacing={2} alignItems="center">
+                <Grid item xs>
+                    <TextField fullWidth name="text" label="Comment" value={formValues.text} onChange={handleChange} />
+                </Grid>
+                <Grid item>
+                    <Button variant="contained" onClick={handleClick} color="success" type="submit">Add</Button>
+                </Grid>
+            </Grid>
+        </Paper>
+    );
+}
