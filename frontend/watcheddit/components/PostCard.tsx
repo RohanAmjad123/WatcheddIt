5a3bbe8e4958ece3909dfbcc247fe225cbc24c79
Rohan Amjad <rohanamjad123@gmail.com>
2022-03-25 02:07:16 -0600

    added PostPage

--- frontend/watcheddit/components/PostCard.tsx
+++ frontend/watcheddit/components/PostCard.tsx
@@ -2,27 +2,29 @@ import { Grid } from "@mui/material";
 import { Typography, Paper } from "@mui/material";
 import PostVotes from './PostVotes'
 import { Post } from '../interfaces/index'
+import Link from 'next/link'
 
 
 export default function PostCard({ post }: { post: Post }) {
-    console.log(post)
-    
+
     return (
         <Grid item>
-        <Paper>
-        <Grid container direction="column" p={2}>
-            <Grid item>
-                <Typography variant="overline">{post.user}</Typography>
-            </Grid>
-            <Grid item>
-                <Typography variant="h6">{post.title}</Typography>
-            </Grid>
-            <Grid item>
-                <Typography variant="body2">{post.description}</Typography>
-            </Grid>
-            <PostVotes votes={post.votes}/>
-        </Grid>
-        </Paper>
-        </Grid>
+            <Link href={`/${encodeURIComponent(post.imdbID)}/${encodeURIComponent(post._id)}`} passHref>
+                <Paper>
+                    <Grid container direction="column" p={2}>
+                        <Grid item>
+                            <Typography variant="overline">{post.user}</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="h6">{post.title}</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="body2">{post.description}</Typography>
+                        </Grid>
+                        <PostVotes votes={post.votes} />
+                    </Grid>
+                </Paper>
+            </Link>
+        </Grid >
     );
 }
---
+++ frontend/watcheddit/pages/[mediaID]/[postID].tsx
@@ -0,0 +1,68 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { Post, Comment } from '../../interfaces/index'
+import PostCard from '../../components/PostCard'
+import { Grid } from '@mui/material'
+import CommentForm from '../../forms/CommentForm'
+import CommentCard from '../../components/CommentCard'
+import { useAppSelector } from '../../app/hooks'
+
+export default function PostPage({ post, commentsList }: { post: Post, commentsList: Comment[] }) {
+    const commentCards = commentsList.map((comment) => <CommentCard key={comment._id} comment={comment} />)
+    const userLoggedIn = useAppSelector((state) => state.loggedIn)
+
+    const userComment = () => {
+        if (userLoggedIn) {
+            return (
+                <Grid item>
+                    <CommentForm />
+                </Grid>
+            )
+        }
+    }
+
+    return (
+        <Grid container direction="column" rowSpacing={3}>
+            <Grid item>
+                <PostCard post={post} />
+            </Grid>
+            {userComment()}
+            <Grid item container direction="column">
+                {commentCards}
+            </Grid>
+        </Grid>
+    );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const postRes = await fetch(`http://localhost:3000/api/posts/`);
+    const postList = await postRes.json();
+
+    const paths = postList.map((post: Post) => ({
+        params: {
+            mediaID: post.imdbID,
+            postID: post._id
+        },
+    }))
+
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    const params = context.params!
+    const postRes = await fetch(`http://localhost:3000/api/media/${params.mediaID}/post/${params.postID}/`);
+    const post = await postRes.json();
+
+    const commentRes = await fetch(`http://localhost:3000/api/${params.postID}/comments`);
+    const commentsList = await commentRes.json();
+
+    return {
+        props: {
+            post,
+            commentsList,
+        }
+    };
+}
+
