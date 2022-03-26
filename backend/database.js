4e660e025798670ee5c3db6390c187450f4078c4
Rohan Amjad <rohanamjad123@gmail.com>
2022-03-26 06:13:50 -0600

    added functionality to test on a duplicate test db

--- backend/database.js
+++ backend/database.js
@@ -1,48 +1,58 @@
-const {MongoClient} = require("mongodb");
+const { MongoClient } = require("mongodb");
 
-const connection = "mongodb+srv://WatchedditApp:SENG401@watchedditcluster.j2xln.mongodb.net/Watcheddit?retryWrites=true&w=majority";
-const client = new MongoClient(connection, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+const env = process.env.NODE_ENV || "development";
+var databaseName;
+
+if (env === "test") {
+  databaseName = 'Watcheddit'
+} else {
+  databaseName = 'Watcheddit'
+}
+
+const connection =
+  "mongodb+srv://WatchedditApp:SENG401@watchedditcluster.j2xln.mongodb.net/Watcheddit?retryWrites=true&w=majority";
+
+  const client = new MongoClient(connection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 let dbConnect;
 // let dbWrite;
 
 module.exports = {
-    connect: async function ( callback ) {
-        console.log("Connecting....")
+  connect: async function (callback) {
+    console.log("Connecting....");
 
-        await client.connect();
-        dbConnect = client.db("Watcheddit")
-        console.log("connected to Watcheddit")
-    },
+    await client.connect();
+    dbConnect = client.db(databaseName);
+    console.log(`connected to ${databaseName}`);
+  },
 
-    isDbConnected: function() {
-        return client.isConnected();
-    },
+  isDbConnected: function () {
+    return client.isConnected();
+  },
 
-    closeConnection: function () {
-        client.close();
-    },
+  closeConnection: function () {
+    client.close();
+  },
 
-    // connectToWrite: function () {
-    //     client.connect(function (err, db) {
-    //         if (err || !db) {
-    //             console.log(err)
-    //             return
-    //         }
-    //         dbConnect = db.db("write");
-    //         console.log("Successfully connected to write MongoDB.");
-    //     });
-    // },
+  // connectToWrite: function () {
+  //     client.connect(function (err, db) {
+  //         if (err || !db) {
+  //             console.log(err)
+  //             return
+  //         }
+  //         dbConnect = db.db("write");
+  //         console.log("Successfully connected to write MongoDB.");
+  //     });
+  // },
 
-    getDb: function () {
-        return dbConnect;
-    },
+  getDb: function () {
+    return dbConnect;
+  },
 
-    // getWriteDb: function () {
-    //     return dbWrite;
-    // }
-
+  // getWriteDb: function () {
+  //     return dbWrite;
+  // }
 };
