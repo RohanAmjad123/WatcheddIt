f7888422cba14c13e3a1cbd3fd72e9533e1e53a3
sajid <Sajid2437@gmail.com>
2022-03-25 18:42:45 -0600

    fixed login tests

--- backend/tests/Login/testLogin.js
+++ backend/tests/Login/testLogin.js
@@ -6,6 +6,7 @@ chai.use(chaiHttp);
 const url = 'http://127.0.0.1:3000/api'
 
 describe('/POST Login', function() {
+  // Test Case 01
   it('Login with valid data', async function() {
     let res = await chai.request(url)
     .post('/login/')
@@ -16,11 +17,12 @@ describe('/POST Login', function() {
     .set('Content-Type', 'application/json')
     expect(res).to.have.status(200);
     expect(res).to.have.a.cookie
-    assert(res.body.username == 'johnnyman', 'The entered username should be the same')
-    assert(res.body.password != 'papadog', 'The password should be hashed')
-    assert(res.body.type == 'user', 'The user should be a regular user')
+    assert.equal(res.body.username,'johnnyman', 'The entered username should be the same')
+    assert.notEqual(res.body.password,'papadog', 'The password should be hashed')
+    assert.equal(res.body.type, 'user', 'The user should be a regular user')
   })
   it('Login with correct username but incorrect password', async function() {
+    // Test Case 02
     let res = await chai.request(url)
     .post('/login/')
     .send({
@@ -30,9 +32,10 @@ describe('/POST Login', function() {
     .set('Content-Type', 'application/json')
     expect(res).to.have.status(400);
     expect(res).to.not.have.a.cookie
-   // assert(res.body == 'Wrong password', 'The response body indicates that the password is incorrect');
+    assert.equal(res.text, 'Wrong password', 'The response body should indicate that the password is incorrect');
   })
   it('Login with a username that does not exist', async function() {
+    // Test Case 03
     let res = await chai.request(url)
     .post('/login/')
     .send({
@@ -42,7 +45,7 @@ describe('/POST Login', function() {
     .set('Content-Type', 'application/json')
     expect(res).to.have.status(400);
     expect(res).to.not.have.a.cookie
-   // assert(res.body == 'Wrong password', 'The response body indicates that the password is incorrect');
+    assert.equal(res.text, 'No matching username found', 'The response body should indicate a matching username does not exist');
   })
 
 })
