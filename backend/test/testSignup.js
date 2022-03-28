1a3c58b04f8fa14c16132f619886a4a42a89007d
Rohan Amjad <rohanamjad123@gmail.com>
2022-03-27 20:47:23 -0600

    removed database test

--- backend/test/testSignup.js
+++ backend/test/testSignup.js
@@ -49,35 +49,36 @@ describe('Signup tests', () => {
   });
 
   // Test Case 05
-  describe(('Check password'), () => {
-    it('should assert that the inserted users password is obfuscated', (done) => {
-      dbConnect = connect.getDb();
-      dbConnect.collection('users').findOne(
-        {
-          username: 'testuser',
-        },
-        (err, res) => {
-          if (err) throw err;
-          assert.notEqual(
-            res.password,
-            'testpassword',
-            'The password should be obfuscated',
-          );
-        },
-      );
+  // SHOULD NOT BE TESTING DATABASE
+  // describe(('Check password'), () => {
+  //   it('should assert that the inserted users password is obfuscated', (done) => {
+  //     dbConnect = connect.getDb();
+  //     dbConnect.collection('users').findOne(
+  //       {
+  //         username: 'testuser',
+  //       },
+  //       (err, res) => {
+  //         if (err) throw err;
+  //         assert.notEqual(
+  //           res.password,
+  //           'testpassword',
+  //           'The password should be obfuscated',
+  //         );
+  //       },
+  //     );
 
-      // Remove the newly created user from the database
-      dbConnect.collection('users').deleteOne(
-        {
-          username: 'testuser',
-        },
-        (err) => {
-          if (err) throw err;
-          done();
-        },
-      );
-    });
-  });
+  //     // Remove the newly created user from the database
+  //     dbConnect.collection('users').deleteOne(
+  //       {
+  //         username: 'testuser',
+  //       },
+  //       (err) => {
+  //         if (err) throw err;
+  //         done()
+  //       },
+  //     );
+  //   });
+  // });
 
   // Test Case 06
   describe(('/POST Signup with an already existing username'), () => {
