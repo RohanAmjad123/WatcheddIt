d3074fa824c48f0fefcedc3fcaa0242d30c9b9a0
sajid <Sajid2437@gmail.com>
2022-03-29 03:39:47 -0600

    linter....

--- backend/load-test/script1.js
+++ backend/load-test/script1.js
@@ -1,12 +1,6 @@
 import http from 'k6/http';
 
 export default function () {
-  const url = 'http://127.0.0.1:3000/api/media/';
-  const params = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
+  const url = 'http://127.0.0.1:3000/api/media/'
   http.get(url)
 }
