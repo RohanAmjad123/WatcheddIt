12c81dee7cdd133715526d7ee805297298d182c5
Rohan Amjad <rohanamjad123@gmail.com>
2022-03-28 18:38:14 -0600

    spacing changes to login and signup

--- frontend/watcheddit/pages/login.tsx
+++ frontend/watcheddit/pages/login.tsx
@@ -4,11 +4,11 @@ import { Typography, Grid, Paper } from "@mui/material";
 export default function login() {
     return (
         <Paper>
-            <Grid container direction="column" p={3} mt={3}>
+            <Grid container direction="column" p={3}>
                 <Grid item>
                     <Typography variant="h4">Log In</Typography>
                 </Grid>
-                <Grid item pt={2}>
+                <Grid item pt={3}>
                     <LoginForm />
                 </Grid>
             </Grid>
