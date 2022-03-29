5573248ea42a8f6c95ba58dd7e6931d6fc6d6c79
Karl Winkler <39600307+KarlWinkler@users.noreply.github.com>
2022-03-29 00:45:41 -0600

    Update putMedia.js

--- backend/put/putMedia.js
+++ backend/put/putMedia.js
@@ -1,7 +1,7 @@
 const connect = require('../database');
 
 exports.putMedia = (req, res) => {
-  if (req.session.user && req.session.user.type == 'admin') {
+  if (req.session.user && req.session.user.type === 'admin') {
     const dbConnect = connect.getDb();
     dbConnect.collection('Media').updateOne({imdbID: req.params.mediaId}, {$set: req.body}, { upsert: true });
     res.sendStatus(200);
