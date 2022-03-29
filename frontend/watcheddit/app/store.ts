d7a82a92bd6a1cf19099b3e3d246e589ad782f76
Rohan Amjad <rohanamjad123@gmail.com>
2022-03-29 17:24:52 -0600

    added state persisting with redux persist

--- frontend/watcheddit/app/store.ts
+++ frontend/watcheddit/app/store.ts
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { userReducer } from "./reducers/userReducer"
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+ 
+const persistConfig = {
+    key: "root",
+    storage
+};
 
+const persistedReducer = persistReducer(persistConfig, userReducer);
+  
 export const store = configureStore({
-    reducer: userReducer
+    reducer: persistedReducer
 })
 
 export type RootState = ReturnType<typeof store.getState>
